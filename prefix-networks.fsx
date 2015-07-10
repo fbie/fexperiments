8a959f29a18c5ced4071820ceffe160fc25a566c
Florian Biermann <fbie@itu.dk>
2015-07-10 10:00:47 +0200

    re-write based on actual paper

--- prefix-networks.fsx
+++ prefix-networks.fsx
@@ -1,4 +1,57 @@
-module List =
+module ParallelPrefix
+
+(* We don't actually use these, but they're helpful for error finging. *)
+type Fan<'a> = 'a list -> 'a list
+type PP<'a> = Fan<'a> -> 'a list -> 'a list
+
+let mkFan f xs =
+  match xs with
+    | [] -> []
+    | x :: xs -> x :: (List.map (f x) xs)
+
+let rec ser f xs =
+  match xs with
+    | [] -> []
+    | [x] -> xs
+    | x :: y :: xs ->
+      let [x'; y'] = f [x; y]
+      x' :: ser f (y' :: xs)
+
+let delay ds =
+  match ds with
+    | [] -> []
+    | [d] -> ds
+    | ds -> let dm = List.max ds + 1 in [ for i in 1..List.length ds -> dm ]
+
+(* Just to avoid writing new 0-arrays all the time... *)
+let checkDelay p n =
+  p delay [ for i in 1..n -> 0 ]
+
+(* For later visualization of networks. *)
+module Net =
+  type Net = { fans: (int * int list) list; wire: int; phase: int }
+
+  let fans n = n.fans
+  let wire n = n.wire
+  let phase n = n.phase
+
+  let netsz n =
+    [ for i in 1..n -> { fans = []; wire = i; phase = 0 } ]
+
+  let netFan is =
+    match is with
+    | [] -> []
+    | [x] -> [x]
+    | x :: xs ->
+      let ph = (List.map phase >> List.max) is
+      let j = { fans = (ph, List.map wire is) :: fans x; wire = wire x; phase = ph + 1 }
+      j :: List.map (fun k -> { fans = k.fans; wire = k.wire; phase = ph + 1 }) xs
+
+  let getNets f n =
+    f netFan (netsz n)
+
+(* Some functions on lists which the standard library does not provide. *)
+module PList =
   let last xs =
     (List.rev >> List.head) xs
 
@@ -11,49 +64,60 @@ module List =
   let skip m xs =
     xs |> List.mapi (fun i x -> if i >= m then Some x else None) |> List.choose id
 
-  let split m xs =
+  (* We could also use List.partition for this, but it'd be a bit more clumsy. *)
+  let splitAt m xs =
     take m xs, skip m xs
 
-module PrefixNetwork =
-  type NW<'a> = ('a list -> 'a list) -> 'a list -> 'a list
+  let toLasts f xs =
+    List.map (fun (l, r) -> l @ [r]) <| List.zip (List.map init xs) (f (List.map last xs))
 
-  let pplus [a; b] =
-    [a; a + b]
+  let toTail f xs =
+    List.head xs :: f (List.tail xs)
 
-  let delF [a; b] =
-    let m = max a b
-    [m; m + 1]
+  let toLast f xs =
+    init xs @ [f (last xs)]
 
-  let rec ser comp xs =
-    match xs with
-      | [] -> []
-      | [x] -> [x]
-      | x :: y :: xs ->
-        let [c1; c2] = comp [x; y]
-        c1 :: ser comp (c2 :: xs)
+  let toInit f xs =
+    f (init xs) @ [last xs]
 
-  let rec fan comp xs =
-    match xs with
-      | [] -> []
-      | x :: xs -> x :: List.map (comp x) xs
+open PList
 
-  let rec skl comp xs =
-    match xs with
-      | [] -> []
-      | [x] -> [x]
-      | _ ->
-        let lxs, rxs = List.split ((List.length xs + 1) / 2) xs
-        let lys = skl comp lxs
-        let rys = skl comp rxs
-        let rys' = fan comp ((List.last lys) :: rys)
-        (List.init lys) @ rys';
+(* Simple Sklansky construction. *)
+let rec skl f xs =
+  match xs with
+    | [] -> []
+    | [x] -> xs
+    | xs ->
+      let lxs, rxs = splitAt ((List.length xs) / 2) xs
+      let lys, rys = skl f lxs, skl f rxs (* Recursive computation. *)
+      init lys @ f (last lys :: rys) (* Combination and propagation to the bottom right. *)
 
-  let buf1 comp xs =
-    match xs with
-      | [] -> []
-      | _ -> (comp [List.head xs]) @ List.tail xs
+(* Brent-Kung pattern *)
+let rec split ds xs =
+  match ds, xs with
+    | [], _ -> []
+    | _, [] -> []
+    | d :: ds, xs ->
+      let lxs, rxs = splitAt d xs
+      lxs :: split ds rxs
 
-  let rec bufall comp xs =
-    match xs with
-      | [] -> []
-      | _ -> buf1 comp (List.head xs :: bufall comp (List.tail xs))
+let shift xs =
+  match xs with
+    | [] -> []
+    | x :: xs ->
+      x - 1 :: init xs @ [last xs + 1]
+
+(* Haskell's f . g corresponds to F#'s f << g. *)
+let build0 ws p f =
+  split ws >> List.map (ser f) >>
+  toInit (toLasts (p f)) >> List.concat >>
+  split (shift ws) >> toTail (List.map f) >> List.concat
+
+let twos n =
+  List.replicate n (n / 2) @ [n % 2]
+
+let rec bK0 f xs =
+  match xs with
+    | [] -> []
+    | [x] -> xs
+    | xs -> build0 (twos (List.length xs)) bK0 f xs
