cc0b40ce513d8de4d18d34e3a1568251910d7163
Florian Biermann <fbie@itu.dk>
2015-07-10 17:08:57 +0200

    reformulate splitAt and split, throw out toLasts

--- prefix-networks.fsx
+++ prefix-networks.fsx
@@ -1,6 +1,9 @@
-module ParallelPrefix
+#if INTERACTIVE
+#else
+module PrefixNetwork
+#endif
 
-(* We don't actually use these, but they're helpful for error finging. *)
+(* We don't actually use these, but they're helpful for error finding. *)
 type Fan<'a> = 'a list -> 'a list
 type PP<'a> = Fan<'a> -> 'a list -> 'a list
 
@@ -50,6 +53,10 @@ module Net =
   let getNets f n =
     f netFan (netsz n)
 
+let check0 p n =
+  let l = [ for i in 1..n -> [i] ]
+  p (mkFan (@)) l = List.tail (List.scan (@) [] l)
+
 (* Some functions on lists which the standard library does not provide. *)
 module PList =
   let last xs =
@@ -58,31 +65,29 @@ module PList =
   let init xs =
     (List.rev >> List.tail >> List.rev) xs
 
-  let take m xs =
-    xs |> List.mapi (fun i x -> if i < m then Some x else None) |> List.choose id
-
-  let skip m xs =
-    xs |> List.mapi (fun i x -> if i >= m then Some x else None) |> List.choose id
-
-  (* We could also use List.partition for this, but it'd be a bit more clumsy. *)
+  (* Robust, tail-recursive list splitting. *)
   let splitAt m xs =
-    take m xs, skip m xs
-
-  let toLasts f xs =
-    List.map (fun (l, r) -> l @ [r]) <| List.zip (List.map init xs) (f (List.map last xs))
-
-  let toTail f xs =
-    List.head xs :: f (List.tail xs)
+    let rec split m lxs rxs =
+      match rxs with
+        | []                 -> List.rev lxs, rxs
+        | x :: xs when m = 0 -> List.rev lxs, rxs
+        | x :: xs            -> split (m - 1) (x :: lxs) xs
+    match xs with
+      | [] -> [], []
+      | x :: xs -> split m [x] xs
 
   let toLast f xs =
     init xs @ [f (last xs)]
+
+  let toTail f xs =
+    List.head xs :: f (List.tail xs)
 
   let toInit f xs =
     f (init xs) @ [last xs]
 
 open PList
 
-(* Simple Sklansky construction. *)
+(* Simple Sklansky construction. TODO: Formulate as tail-recursive. *)
 let rec skl f xs =
   match xs with
     | [] -> []
@@ -92,14 +97,23 @@ let rec skl f xs =
       let lys, rys = skl f lxs, skl f rxs (* Recursive computation. *)
       init lys @ f (last lys :: rys) (* Combination and propagation to the bottom right. *)
 
-(* Brent-Kung pattern *)
-let rec split ds xs =
+(* Slow and prone to stack overflows. *)
+let rec split2 ds xs =
   match ds, xs with
-    | [], _ -> []
-    | _, [] -> []
+    | [], _ -> [xs]
     | d :: ds, xs ->
       let lxs, rxs = splitAt d xs
-      lxs :: split ds rxs
+      lxs :: split2 ds rxs
+
+(* TODO: No stack overflows since tail-recusrive, but still slow. *)
+let split ds xs =
+  let rec spl ds xs ss =
+    match ds, xs with
+      | [], _ -> List.rev ss
+      | d :: ds, xs ->
+        let lxs, rxs = splitAt d xs
+        spl ds rxs (lxs :: ss)
+  spl ds xs []
 
 let shift xs =
   match xs with
@@ -110,12 +124,13 @@ let shift xs =
 (* Haskell's f . g corresponds to F#'s f << g. *)
 let build0 ws p f =
   split ws >> List.map (ser f) >>
-  toInit (toLasts (p f)) >> List.concat >>
+  toInit (toLast (p f)) >> List.concat >>
   split (shift ws) >> toTail (List.map f) >> List.concat
 
 let twos n =
   List.replicate n (n / 2) @ [n % 2]
 
+(* Brent-Kung pattern *)
 let rec bK0 f xs =
   match xs with
     | [] -> []
