12b735c88cd3240e7be4ff750b6a71eacb80ab80
Florian Biermann <fbie@itu.dk>
2016-02-03 15:10:57 +0100

    overload >> for lens composition

--- lens.fsx
+++ lens.fsx
@@ -19,11 +19,12 @@ let compSet f g a c = set f a (set g (get f a) c)
 
 (* Get and update nested data structures via composed lense. *)
 let compLens f g = { get = compGet f g; set = compSet f g }
+let (>>) f g = compLens f g
 
-let fstSnd<'a, 'b> = compLens fstLens sndLens
-let sndFst<'a, 'b> = compLens sndLens fstLens
-let fstFst<'a, 'b> = compLens fstLens fstLens
-let sndSnd<'a, 'b> = compLens sndLens sndLens
+let fstSnd<'a, 'b> = fstLens >> sndLens
+let sndFst<'a, 'b> = sndLens >> fstLens
+let fstFst<'a, 'b> = fstLens >> fstLens
+let sndSnd<'a, 'b> = sndLens >> sndLens
 
 (* Check that lens rules hold. *)
 set fstLens (1, 2) 3 = set fstLens (set fstLens (1, 2) 4) 3
